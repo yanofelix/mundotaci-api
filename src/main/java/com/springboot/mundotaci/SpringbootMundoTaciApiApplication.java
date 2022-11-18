16df8bd9dd60dffa561b5d72478c1c0579e1c922
yanofelix <devyanfelix@gmail.com>
2022-11-18 14:36:51 -0300

    first commit

---
+++ src/main/java/com/springboot/mundotaci/SpringbootMundoTaciApiApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.mundotaci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMundoTaciApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMundoTaciApiApplication.class, args);
+	}
+
+}
